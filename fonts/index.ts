dd1bf2ecc59891de7b88584bf60ac4e53192375c
Manoj Kumar <manojkumararumainathan@gmail.com>
2025-11-25 14:49:35 +0530

    build issue font

--- fonts/index.ts
+++ fonts/index.ts
@@ -5,7 +5,7 @@ export const grift = localFont({
         {path:"./Grift/Grift-Light.otf",weight:"300", style:"normal"},
         {path:"./Grift/Grift-Regular.otf",weight:"400", style:"normal"},
         {path:"./Grift/Grift-Medium.otf",weight:"500", style:"normal"},
-        {path:"./Grift/Grift-Semibold.otf",weight:"600", style:"normal"},
+        {path:"./Grift/Grift-SemiBold.otf",weight:"600", style:"normal"},
         {path:"./Grift/Grift-Bold.otf",weight:"700", style:"normal"},
     ],
     variable: "--font-grift",
